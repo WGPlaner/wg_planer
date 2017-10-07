9870b757508f80805f25979937fc05cf6da5cae6
ArneSchulze <arneschulze98@gmail.com>
2017-10-07 14:59:41 +0200

    Revert "Added DataContainer.User class"
    This reverts commit 7c8327c434c2ca6b096824155937677da9711578.

--- Client/app/src/main/java/de/ameyering/wgplaner/wgplaner/utils/DataContainer.java
+++ Client/app/src/main/java/de/ameyering/wgplaner/wgplaner/utils/DataContainer.java
@@ -1,12 +1,9 @@
 package de.ameyering.wgplaner.wgplaner.utils;
 
-import android.support.annotation.Nullable;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import de.ameyering.wgplaner.wgplaner.structure.Item;
-import de.ameyering.wgplaner.wgplaner.structure.User;
 
 public abstract class DataContainer {
 
@@ -36,28 +33,6 @@ public abstract class DataContainer {
 
         public static SimpleDateFormat getDateFormat(){
             return dateFormat;
-        }
-    }
-
-    public static abstract class User {
-        private static ArrayList<de.ameyering.wgplaner.wgplaner.structure.User> users = new ArrayList<>();
-
-        public static ArrayList<de.ameyering.wgplaner.wgplaner.structure.User> getUser() {
-            return users;
-        }
-
-        protected static void addUser(de.ameyering.wgplaner.wgplaner.structure.User user){
-            users.add(user);
-        }
-
-        @Nullable
-        protected static String getDisplayNameByUid(String uid){
-            de.ameyering.wgplaner.wgplaner.structure.User user = new de.ameyering.wgplaner.wgplaner.structure.User(uid, "");
-            if(users.contains(user)){
-                int index = users.indexOf(user);
-                return users.get(index).getDisplayName();
-            }
-            return null;
         }
     }
 }
