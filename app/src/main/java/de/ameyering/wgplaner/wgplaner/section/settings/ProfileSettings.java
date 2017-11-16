3712242b77c5f4a67b5173f31c07a4045b2f3c03
Nina <32509410+NinaWieland@users.noreply.github.com>
2017-11-16 12:21:12 +0100

    [Profile] basics for the layout

---
+++ app/src/main/java/de/ameyering/wgplaner/wgplaner/section/settings/ProfileSettings.java
@@ -0,0 +1,47 @@
+package de.ameyering.wgplaner.wgplaner.section.settings;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import de.ameyering.wgplaner.wgplaner.R;
+import de.ameyering.wgplaner.wgplaner.section.home.AddItemActivity;
+
+public class ProfileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profile_settings_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_close_white);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileSettings.this);
+                builder.setMessage(getString(R.string.dialog_discard_message));
+                builder.setPositiveButton(R.string.dialog_discard_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        setResult(RESULT_CANCELED);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.dialog_discard_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+}
