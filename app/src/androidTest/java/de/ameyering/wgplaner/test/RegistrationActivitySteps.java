a2113055ec393c1167363a2220bd0abb02e84ab5
Andre Meyering <info@andremeyering.de>
2018-06-01 14:13:09 +0200

    Fix UI tests

--- app/src/androidTest/java/de/ameyering/wgplaner/test/RegistrationActivitySteps.java
+++ app/src/androidTest/java/de/ameyering/wgplaner/test/RegistrationActivitySteps.java
@@ -12,7 +12,6 @@ import cucumber.api.java.en.Given;
 
 import de.ameyering.wgplaner.test.mockclasses.TestServerCalls;
 import de.ameyering.wgplaner.wgplaner.section.registration.RegistrationActivity;
-import de.ameyering.wgplaner.wgplaner.structure.Money;
 import de.ameyering.wgplaner.wgplaner.utils.Configuration;
 import de.ameyering.wgplaner.wgplaner.utils.DataProvider;
 import de.ameyering.wgplaner.wgplaner.utils.ImageStore;
@@ -23,7 +22,6 @@ public class RegistrationActivitySteps {
 
     private void initActivity(Activity activity) {
         Context appContext = activity.getBaseContext();
-        Money.initialize(Locale.getDefault());
         ImageStore.initialize(appContext);
         Configuration.initConfig(appContext);
         DataProvider.getInstance().setServerCallsInstance(TestServerCalls.getInstance());
